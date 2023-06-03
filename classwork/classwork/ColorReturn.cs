e82cfc2173a821a016ad548cd0628ff831b865da
Habib-Gulomov <gulomovhabibulloh@gmail.com>
2023-06-03 19:26:25 +0500

    check task

---
+++ classwork/classwork/ColorReturn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork
+{
+
+
+    internal static class ColorReturn
+    {
+        public static int ColorNumReturn(int color)
+        {
+            return color;
+
+        }
+    }
+}
---
+++ classwork/classwork/Program.cs
@@ -0,0 +1,37 @@
+﻿using classwork;
+using System.Collections;
+using System.Collections.Generic;
+
+//ArrayList arList = new ArrayList()
+//{
+//    1,"bill",300,4.5F
+//};
+//foreach (var item in  arList)
+//{
+
+//    Console.WriteLine(item);
+//}
+
+//for (int i = 0; i < arList.Count; i++)
+//{
+//    Console.WriteLine(arList[i]);
+//}
+
+
+var colors = new Dictionary<string, int>
+{
+    {"black",0 },
+    {"brown",1 },
+    {"red",2 },
+    {"orange",3 },
+    {"yellow",4 },
+    {"green",5 },
+    {"blue",6 },
+    {"violet",7 },
+    {"grey",8 },
+    {"white",9 }
+};
+var inner = colors["grey"]; // type color
+var result = ColorReturn.ColorNumReturn(colors["black"]);
+
+Console.WriteLine(result);
